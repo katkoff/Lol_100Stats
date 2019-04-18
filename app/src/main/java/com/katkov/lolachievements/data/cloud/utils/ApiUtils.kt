2339a56b95bfe69ab1c1992dad9a5061b3faa762
adv.katkov <adv.katkov@gmail.com>
2019-04-18 21:40:46 +0700

    update apikey

--- app/src/main/java/com/katkov/lolachievements/data/cloud/utils/ApiUtils.kt
+++ app/src/main/java/com/katkov/lolachievements/data/cloud/utils/ApiUtils.kt
@@ -2,6 +2,6 @@ package com.katkov.lolachievements.data.cloud.utils
 
 object ApiUtils {
 
-    const val API_KEY = "RGAPI-ce95397e-3e59-4e37-948b-588046fd8429"
+    const val API_KEY = "RGAPI-95024174-6430-4e0c-8332-723198af36c6"
     const val BASE_URL = "https://api.riotgames.com/lol/"
 }
